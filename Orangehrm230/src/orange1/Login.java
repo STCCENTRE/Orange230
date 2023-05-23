7204fbd31dfb5044c73a18fcce449531312663c3
Mithra <mithrass1997@gmail.com>
2023-05-23 14:35:26 +0530

    first commit by mithra

--- Orangehrm230/src/orange1/Login.java
+++ Orangehrm230/src/orange1/Login.java
@@ -1,10 +1,29 @@
 package orange1;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//https://github.com/STCCENTRE/Orange230
+//token of repository
+//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
 public class Login {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//class for orangehrm loginpage
+		System.out.println("Launch Firefox");
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\mithr\\Downloads\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(40,TimeUnit.SECONDS);
+		driver.get("http://localhost/Orangehrm/orangehrm-5.4/orangehrm-5.4/web/index.php/auth/login");
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("Stcmithra@123");
+		driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
+		driver.findElement(By.xpath("/html/body/div/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a/span")).click();
+		driver.findElement(By.linkText("Add Employee")).click();
+		driver.findElement(By.name("firstName")).sendKeys("Mithra");
+		driver.findElement(By.name("middleName")).sendKeys("S");
+		driver.findElement(By.name("lastName")).sendKeys("S");
+		driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div/div/form/div[2]/button[2]")).click();
 	}
-
 }
