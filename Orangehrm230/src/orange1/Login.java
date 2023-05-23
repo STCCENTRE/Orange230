371b270a5de2d40c444ca16fd9b220d9b54d7209
Rahul <rahulkrishnanus97@gmail.com>
2023-05-23 16:07:20 +0530

    code updated

--- Orangehrm230/src/orange1/Login.java
+++ Orangehrm230/src/orange1/Login.java
@@ -23,6 +23,15 @@ System.setProperty("webdriver.chrome.driver","D:\\automation tools\\chromedriver
 	    
 		driver.findElement(By.name("password")).sendKeys("admin123");
 		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a")).click();
+	    
+		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[2]/div[1]/button")).click();
+	   
+		driver.findElement(By.name("firstName")).sendKeys("RAHUL");
+	    
+		driver.findElement(By.name("middleName")).sendKeys("KRISHNAN");
+	    
+		driver.findElement(By.name("lastName")).sendKeys("U S");
 		}
 
 }
