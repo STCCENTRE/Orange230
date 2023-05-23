9a56ee00d3d68f42c0184aab7960a7d05c945810
pranav9858 <pranavpranav932@gmail.com>
2023-05-23 14:42:42 +0530

    commit by pranav

--- Orangehrm230/src/orange1/Login.java
+++ Orangehrm230/src/orange1/Login.java
@@ -1,10 +1,31 @@
 package orange1;
 
-public class Login {
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Login {
+	@Test(priority=0)
+    public void orange02() {
+   	 System.out.println("Launching ffirefox");
+        System.setProperty("webdriver.gecko.driver","E:\\Downloads\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS) ;
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().window().maximize() ;
+        
+        driver.findElement(By.name("username")).sendKeys("Admin");
+        driver.findElement(By.name("password")).sendKeys("admin123");         
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
+	}
+        
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//class for orangehrm loginpage
+			// TODO Auto-generated method stub
+	//class for orangehrm loginpage
 	}
 
 }
